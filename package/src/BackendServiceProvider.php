76ec2b5242c37ad05ab50372be9254098b2322cf
Scott Bedard <scottbedard90@gmail.com>
2022-03-22 18:29:54 -0500

    install local package

---
+++ package/src/BackendServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Bedard\Backend;
+
+use Illuminate\Support\ServiceProvider;
+
+class BackendServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+      // ...
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+      // ...
+    }
+}
