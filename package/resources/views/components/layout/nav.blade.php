47faef4a0a19e0621453ceaae7e63ee1a5f5625c
Scott Bedard <scottbedard90@gmail.com>
2022-05-24 23:01:30 -0500

    align links to top

--- package/resources/views/components/layout/nav.blade.php
+++ package/resources/views/components/layout/nav.blade.php
@@ -1,11 +1,13 @@
-<nav {{ $attributes->class(['grid gap-6 p-6']) }}>
-    @foreach (Backend::resources() as $resource)
-        <a
-            class="flex gap-3 items-center"
-            href="{{ route('backend.resources.show', ['id' => $resource::$id]) }}">
-            <x-backend::icon :name="$resource::$icon" />
+<nav {{ $attributes->class(['p-6']) }}>
+    <div class="gap-6 grid">
+        @foreach (Backend::resources() as $resource)
+            <a
+                class="flex gap-3 items-center w-full"
+                href="{{ route('backend.resources.show', ['id' => $resource::$id]) }}">
+                <x-backend::icon :name="$resource::$icon" />
 
-            {{ $resource::$title }}
-        </a>
-    @endforeach
+                {{ $resource::$title }}
+            </a>
+        @endforeach
+    </div>
 </nav>
