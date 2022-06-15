cb097f6ed064a4e8ce06da0901b62f5504af2339
Scott Bedard <scottbedard90@gmail.com>
2022-06-14 23:56:44 -0500

    cleanup

--- application/resources/views/login.blade.php
+++ application/resources/views/login.blade.php
@@ -3,9 +3,33 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script src="https://unpkg.com/@bedard/gradient@0.1.2/dist/gradient.bundle.js"></script>
+
         <title>Backend</title>
+
+        <link rel="icon" href="https://fav.farm/🔥" />
     </head>
-    <body>
-        Please log in
+    <body class="items-center justify-center">
+        <canvas class="fixed h-screen w-screen" id="bg"></canvas>
+            
+        <div class="fixed flex h-screen items-center justify-center left-0 p-6 top-0 w-screen">
+            <form class="bg-white drop-shadow-lg p-6 rounded-md max-w-xs w-full">
+                Hello world
+            </form>
+        </div>
+
+        <script>
+            new Gradient(document.getElementById('bg'), {
+                colors: [
+                    '#3b82f6',
+                    '#14b8a6',
+                    '#fb7185',
+                    '#f8fafc',
+                ],
+                seed: Math.random(),
+            })
+        </script>
     </body>
 </html>
