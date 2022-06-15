e45c643d769a474de6dc008e2c97cb5117c2f8db
Scott Bedard <scottbedard90@gmail.com>
2022-06-14 23:15:48 -0500

    scaffolding

--- application/routes/web.php
+++ application/routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,7 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $user = User::factory()->create();
-    
-    return json_encode($user->toArray());
+    return view('login');
 });
