0c07c23dabb4d8f6b0153ff0728855dd071aa772
Scott Bedard <scottbedard90@gmail.com>
2022-06-18 20:40:30 -0500

    cleanup

--- application/routes/api.php
+++ application/routes/api.php
@@ -1,8 +1,6 @@
 <?php
 
-use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,34 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-Route::get('/envoyer', function () {
-    $message = Redis::get('envoyer_status') ?: 'unknown';
-
-    $timeout = intval(Redis::get('envoyer_timeout')) ?: 0;
-
-    $color = 'lightgrey';
-    if ($message === 'unknown') $color = 'yellow';
-    if ($message === 'deploying') $color = 'blue';
-    if ($message === 'deployed') $color = 'brightgreen';
-    if ($message === 'failed') $color = 'red';
-
-    $now = Carbon::now()->getTimestamp();
-
-    if ($timeout > 0 && $now > $timeout) {
-        $color = 'red';
-        $message = 'failed';
-    }
-
-    return [
-        'color' => $color,
-        'label' => 'Envoyer',
-        'message' => $message,
-        'now' => $now,
-        'schemaVersion' => 1,
-        'timeout' => $timeout,
-    ];
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
 });
