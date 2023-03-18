f8b1933e74cb90fa9a27d1733a443948cb6b22a8
Scott Bedard <scottbedard90@gmail.com>
2023-03-18 11:03:39 -0500

    install local package

--- sandbox/config/app.php
+++ sandbox/config/app.php
@@ -185,6 +185,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Bedard\Backend\BackendServiceProvider::class,
 
         /*
          * Application Service Providers...
