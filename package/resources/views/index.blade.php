a513f1e014373074438588a986f927f04744ebb3
Scott Bedard <scottbedard90@gmail.com>
2023-03-18 12:05:36 -0500

    wip

--- package/resources/views/index.blade.php
+++ package/resources/views/index.blade.php
@@ -1,8 +1,12 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
   <head>
+    <title>{{ config('app.name', '') }} backend</title>
+    <meta charset="utf-8">
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=quicksand:400" rel="stylesheet" />
   </head>
   <body>
-    Hello
+    Hello from the backend!
   </body>
 </html>
