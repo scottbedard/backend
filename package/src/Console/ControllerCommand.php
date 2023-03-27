81a3ae0be963739e3600269adabf2f72b3ec357e
Scott Bedard <scottbedard90@gmail.com>
2023-03-27 00:30:38 -0500

    scaffolding the scaffolding

--- package/src/Console/ControllerCommand.php
+++ package/src/Console/ControllerCommand.php
@@ -3,28 +3,12 @@
 namespace Bedard\Backend\Console;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
+use Symfony\Component\Yaml\Yaml;
 
 class ControllerCommand extends Command
 {
-    /**
-     * Construct.
-     *
-     * @var string
-     */
-    public function __construct()
-    {
-        $this->description = trans('backend::console.controller.description');
-
-        $this->signature = '
-            backend:controller {name}
-            {--d|docs : ' . trans('backend::console.controller.docs') . '}
-            {--m|model : ' . trans('backend::console.controller.model') . '}
-            {--t|terse : ' . trans('backend::console.controller.terse') . '}
-        ';
-
-        parent::__construct();
-    }
-
     /**
      * The console command description.
      *
@@ -33,10 +17,68 @@ class ControllerCommand extends Command
     protected $description = 'Create a backend controller';
 
     /**
+     * Construct.
+     *
+     * @var string
+     */
+    public function __construct()
+    {
+        $this->signature = '
+            backend:controller {name}
+            {--f|force : Overwrite existing files}
+            {--d|docs : Add documentation comments}
+        ';
+
+        parent::__construct();
+    }
+
+    /**
      * Execute the console command.
      */
     public function handle(): void
     {
-        //
+        $stub = File::get(__DIR__ . '/stubs/controller.yaml.stub');
+
+        $output = '';
+        $lines = explode(PHP_EOL, $stub);
+
+        $name = $this->argument('name');
+        $singular = strtolower(Str::singular($name));
+        $plural = strtolower(Str::plural($name));
+            
+        foreach ($lines as $line) {
+            // remove comments
+            if (!$this->option('docs') && str_contains($line, '#')) {
+                continue;
+            }
+            
+            $output .= $line . PHP_EOL;
+        }
+
+        // create output directory if necessary
+        $dir = config('backend.backend_directory');
+
+        if (!File::isDirectory($dir)) {
+            File::makeDirectory($dir);
+        }
+
+        // replace entities
+        $output = str_replace(':singular', $singular, $output);
+        $output = str_replace(':plural', $plural, $output);
+
+        // output the file
+        $path = $dir . '/' . $plural . '.yaml';
+
+        if (!$this->option('force') && File::exists($path)) {
+            if (!$this->confirm('Controller "' . $plural . '.yaml" already exists. Overwrite?')) {
+                return;
+            }
+        }
+
+        $filename = $plural . '.yaml';
+
+        File::put($dir . '/' . $filename, $output);
+
+        $this->info('Created ' . $plural . ' controller!');
     }
 }
