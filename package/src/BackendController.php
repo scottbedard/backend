c8efc0ef0c65a3d9c4dca2e40f909e73a57d6d5c
Scott Bedard <scottbedard90@gmail.com>
2023-03-28 22:28:21 -0500

    wip

--- package/src/BackendController.php
+++ package/src/BackendController.php
@@ -34,7 +34,9 @@ class BackendController extends BaseController
         ]);
 
         return Backend::view([
+            'config' => $config,
             'page' => $page,
+            'route' => $routeName,
         ]);
     }
 }
