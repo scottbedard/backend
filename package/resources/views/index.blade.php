63ef57754b5748fbe7374382ad886bf7f9a93f57
Scott Bedard <scottbedard90@gmail.com>
2023-04-16 22:21:42 -0500

    wording

--- package/resources/views/index.blade.php
+++ package/resources/views/index.blade.php
@@ -1,10 +1,10 @@
 <x-backend::layout>
   <div class="p-6">
     <x-backend::banner
-      header="Uh oh, there is no index route defined!"
+      header="Uh oh, no index route is defined!"
       icon="alert-triangle">
-      Don't worry, this is an easy fix! Create a <code class="bg-gray-200 inline-block px-1 rounded text-sm">_root.yaml</code>
-      and define a route with path <code class="bg-gray-200 inline-block px-1 rounded text-sm">/</code>.
+      Don't worry, this is an easy fix! Create an <code class="bg-gray-200 inline-block px-1 rounded text-sm">_index.yaml</code> file
+      and define a route with a path of <code class="bg-gray-200 inline-block px-1 rounded text-sm">null</code>.
     </x-backend::banner>
   </div>
 </x-backend::layout>
