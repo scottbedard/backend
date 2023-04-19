2c311088617e9c6f4410e7c08a0758aac201779d
Scott Bedard <scottbedard90@gmail.com>
2023-04-18 20:39:50 -0500

    ci

---
+++ sandbox/cypress.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'cypress'
+import setupNodeEvents from './tests/cypress/plugins/index'
+
+export default defineConfig({
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 5000,
+  e2e: {
+    baseUrl: process.env.GITHUB_ACTION
+      ? 'http://localhost:8000'
+      : 'http://localhost',
+    setupNodeEvents(on, config) {
+      return setupNodeEvents(on, config)
+    },
+    specPattern: 'tests/cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'tests/cypress/support/index.js',
+  },
+  fixturesFolder: 'tests/cypress/fixture',
+  retries: 2,
+  screenshotsFolder: 'tests/cypress/screenshots',
+  videosFolder: 'tests/cypress/videos',
+  watchForFileChanges: false,
+})
--- sandbox/tests/cypress/plugins/index.ts
+++ sandbox/tests/cypress/plugins/index.ts
@@ -1,4 +1,6 @@
-/// <reference types="cypress" />
+import swapEnv from './swap-env'
+
+// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
 //
@@ -15,9 +17,9 @@
 /**
  * @type {Cypress.PluginConfig}
  */
-module.exports = (on, config) => {
+export default (on, config) => {
     // `on` is used to hook into various events Cypress emits
     // `config` is the resolved Cypress config
 
-    on('task', require('./swap-env'));
+    on('task', swapEnv);
 };
