906d813a6a0ccc9d159d09f1f12d3dc5f5a84300
Scott Bedard <scottbedard90@gmail.com>
2023-05-07 10:44:11 -0500

    blade

--- package/resources/views/missing-plugin.blade.php
+++ package/resources/views/missing-plugin.blade.php
@@ -1,3 +1,8 @@
 <x-backend::layout padded>
-  There is no view associated with this route.
+  <x-backend::banner
+    header="Plugin view not found"
+    icon="alert-triangle">
+    Don't worry, this is an easy fix! Create a <code class="bg-gray-200 inline-block px-1 rounded text-sm">_root.yaml</code>
+    file and define a route with a path of <code class="bg-gray-200 inline-block px-1 rounded text-sm">/</code>
+  </x-backend::banner>
 </x-backend::layout>
