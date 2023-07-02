9285b09c89daefeb06229af7b5fc700400af5390
Scott Bedard <scottbedard90@gmail.com>
2023-07-01 23:43:26 -0500

    add unknown field view

---
+++ package/resources/views/form/unknown-field.blade.php
@@ -0,0 +1,7 @@
+<div class="flex gap-x-2">
+  <x-backend::icon name="alert-triangle" />
+
+  <div>
+    <pre class="inline value">{{ $id }}</pre> does not have a <pre class="inline value">render</pre> function
+  </div>
+</div>
--- package/src/Config/Plugins/Form/FieldType.php
+++ package/src/Config/Plugins/Form/FieldType.php
@@ -5,6 +5,7 @@ namespace Bedard\Backend\Config\Plugins\Form;
 use Bedard\Backend\Config\Config;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\View\View;
+use Bedard\Backend\Config\Plugins\Form\Field;
 
 class FieldType extends Config
 {
@@ -13,10 +14,12 @@ class FieldType extends Config
      *
      * @param  Model  $model
      *
-     * @return Illuminate\View\View|string
+     * @return Illuminate\View\View
      */
-    public function render(?Model $model = null): View|string
+    public function render(?Model $model = null): View
     {
-        return 'field';
+        return view('backend::form.unknown-field', [
+            'id' => $this->id,
+        ]);
     }
 }
